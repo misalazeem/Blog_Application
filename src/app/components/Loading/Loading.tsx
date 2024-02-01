70f6d8355b4be7acf786d06c08dc9e469bdc1422
Misal Azeem <misal.azeem@gmail.com>
2024-02-02 01:09:32 +0500

    fixing issues

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Loading: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-64">
-      <div className="animate-spin rounded-full border-t-4 border-teal-500 border-solid h-16 w-16"></div>
+      <div className="animate-spin rounded-full border-t-4 border-gray-500 border-solid h-16 w-16"></div>
     </div>
   );
 };
